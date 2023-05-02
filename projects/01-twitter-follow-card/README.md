fae187c7042461df37d02d789e9676cfeeb944ce
GonzalezNacho <nacho.ldm63@gmail.com>
2023-05-02 20:45:17 -0300

    cambiando el nombre a la carpeta del twitter follow card

---
+++ projects/01-twitter-follow-card/README.md
@@ -0,0 +1,5 @@
+Ejercicio 1
+
+Twitter follow card
+
+deploy: [**Twitter follow card**](https://nacho-react-01.surge.sh)
---
+++ projects/01-twitter-follow-card/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard'
+
+export function App () {
+    return(
+        <section className='App'>
+            <TwitterFollowCard userName= "midudev" initialIsFollowing={true}>
+                Miguel Ángel Duran
+            </TwitterFollowCard>
+
+            <TwitterFollowCard userName= "pheralb">
+                Pablo Hernandez
+            </TwitterFollowCard>
+        </section>
+    )
+}
